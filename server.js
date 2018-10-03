16a3aa27a47f73cd15fdf53911d8a825ae6cd160
SirSerje <sir.serje@gmail.com>
2018-10-03 22:14:07 +0300

    bugfix/ #1 empty body comes on post

--- server.js
+++ server.js
@@ -25,62 +25,62 @@ var config = {
 
 firebase.initializeApp(config);
 var database = firebase.database();
-// var all = database.ref().once('value').then(function (snapshot) {
-//   console.log(snapshot.val().movie);
-// })
 
+//variable, which holds highest ID
+var LAST_ID = -1;
+
+database.ref().once('value').then(function (snapshot) {
+   LAST_ID = Math.max.apply(null, Object.keys(snapshot.val().movie));
+})
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-// routes(app);
 // middleware to use for all requests
 router.use(function (req, res, next) {
   // do logging
   console.log('Something is happening.');
-  next(); // make sure we go to the next routes and don't stop here
+  next();
 });
 
+//DEFAULT ROUTE
 router.get('/', function (req, res) {
   res.json({message: 'hooray! welcome to our api!'});
 });
 
+//ADD
 router.route('/movies').post(function (req, res) {
 
-console.log('! body', req.body)
-console.log('! params', req.params)
-   //FIXME: requests body returns empty object instead data.Check postman also
-  /*firebase.database().ref('movie/' + 'movie_1').set({
-    test  :'!!'
-  }, function(error) {
+  firebase.database().ref('movie/' + LAST_ID++).set(
+    req.body.movie, function(error) {
     if (error) {
       res.json({message: 'ERROR'});
     } else {
       res.json({message: 'movie created!'});
     }
-  })*/
-  res.json({message: 'movie created!'});
-
-
+  })
+//GET ALL
 }).get(function (req, res) {
   database.ref().once('value').then(function (snapshot) {
     res.json(snapshot.val().movie);
   })
 });
 
+//GET BY ID
 router.route('/movies/:movieId')
   .get(function (req, res) {
     res.json({message: `${req.params.movieId} get this movie`});
   })
+  //UPDATE
   .put(function (req, res) {
     res.json({message: `${req.params.movieId} update this movie`});
   })
+  //DELETE
   .delete(function (req, res) {
     res.json({message: `${req.params.movieId} movie deleted`});
   });
 
 app.use('/api', router);
-
 
 const server = app.listen(PORT, function () {
   console.log("app running on port.", server.address().port);
