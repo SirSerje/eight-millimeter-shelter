1785141fd3e48b4b015899a6108d7465fedb0f3b
SirSerje <sir.serje@gmail.com>
2018-10-04 22:52:14 +0300

    update utils

---
+++ src/utils/index.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+export const getAllRequest = () => axios.get()
+
+export const getByIdRequest = id => axios.get()
+
+export const addNewRequest = body => axios.post()
+
+export const movieDeleteRequest = id => axios.delete()
+
+export const updateExistingRequest = (id, body) => axios.put()
