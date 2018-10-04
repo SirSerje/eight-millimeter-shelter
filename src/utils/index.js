a85a450c6f4934c55028d312cf403b3f16d4d28e
SirSerje <sir.serje@gmail.com>
2018-10-05 00:04:40 +0300

    update requests

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
-export const getAllRequest = () => axios.get(`api/movies`)
+export const getAllRequest = () => axios.get('api/movies')
 
-export const getByIdRequest = id => axios.get(`api/movies/0`)
+export const getByIdRequest = id => axios.get(`api/movies/${id}`)
 
-export const addNewRequest = body => axios.post('/api/movies', { movie:{foo:'bar'} }) //FIXME: update properly
+export const addNewRequest = (body = {movie:{}}) => axios.post('/api/movies', body)
 
 export const movieDeleteRequest = id => axios.delete()
 
