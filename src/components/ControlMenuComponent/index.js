ad2512516bbaa44035027591e581d63b267b5cae
Serhii Vyazkov <sir.serje@gmail.com>
2018-11-08 09:42:35 +0200

    update react version, use hooks, [WIP]

--- src/components/ControlMenuComponent/index.js
+++ src/components/ControlMenuComponent/index.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useState, useEffect } from 'react';
 import '../../styles/index.scss';
 import ReactFileReader from 'react-file-reader';
 import Button from '@material-ui/core/Button';
@@ -11,193 +12,187 @@ import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import styles from './styles';
 
+function Index (props) {
+  const [byNameInput, setbyNameInput] = useState('');
+  const [byActorInput, setbyActorInput] = useState('');
+  // const [byIdInput, setbyIdInput] = useState(-1);
 
-class Index extends Component {
-  constructor(params) {
-    super(params);
-    this.state = {
-      byNameInput: '',
-      byActorInput: '',
-      byIdInput: -1,
-      addItem: {
-        title: '',
-        release: '',
-        format: '',
-        stars: '',
-      },
-    };
-    this.byNameInputHandler = this.byNameInputHandler.bind(this);
-    this.byActorInputHandler = this.byActorInputHandler.bind(this);
-    this.addNewHandler = this.addNewHandler.bind(this);
-  }
-  addNewHandler(event) {
+  const [addItem, setAddItem] = useState({
+    title: '',
+    release: '',
+    format: '',
+    stars: '',
+  });
+
+  const addNewHandler = event => {
     let fieldType = event.target.name;
-    let temp = this.state.addItem;
+    let temp = addItem;
 
     temp[fieldType] = event.target.value;
-    this.setState({ addItem: temp });
-  }
 
-  byNameInputHandler(event) {
-    this.setState({ byNameInput: event.target.value });
-  }
+    setAddItem({addItem: temp});
+  };
 
-  byActorInputHandler(event) {
-    this.setState({ byActorInput: event.target.value });
-  }
-  render() {
-    const {
-      classes,
-      addHandler,
-      byNameHandler,
-      sortDownHandler,
-      sortUpHandler,
-      handleFiles,
-      initHandler,
-      getAllHandler,
-      byActorHandler,
-    } = this.props;
-    return (
-      <div>
-        <ExpansionPanel defaultExpanded={false}>
-          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-            <div className={classes.column}>
-              <Typography className={classes.heading}>Expand</Typography>
-            </div>
-            {/*<div className={classes.column}>
+  const byNameInputHandler = event => {
+    setbyNameInput(event.target.value);
+  };
+
+  const byActorInputHandler = event => {
+    setbyActorInput(event.target.value);
+  };
+
+  const {
+    classes,
+    addHandler,
+    byNameHandler,
+    sortDownHandler,
+    sortUpHandler,
+    handleFiles,
+    initHandler,
+    getAllHandler,
+    byActorHandler,
+  } = props;
+
+  return (
+    <div>
+      <ExpansionPanel defaultExpanded={false}>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <div className={classes.column}>
+            <Typography className={classes.heading}>Expand</Typography>
+          </div>
+          {/*<div className={classes.column}>
               <Typography className={classes.secondaryHeading}>Use controls to operate with API stuff</Typography>
             </div>*/}
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails className={classes.details}>
-            <div className={classes.column}>
-              <Button
-                variant="outlined"
-                color="secondary"
-                className={classes.button}
-                onClick={initHandler}
-              >
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails className={classes.details}>
+          <div className={classes.column}>
+            <Button
+              variant="outlined"
+              color="secondary"
+              className={classes.button}
+              onClick={initHandler}
+            >
                 init
-              </Button>
+            </Button>
+            <Button
+              variant="outlined"
+              color="secondary"
+              className={classes.button}
+              onClick={getAllHandler}
+            >
+                getAll
+            </Button>
+            {props.block === 1 ? (
               <Button
                 variant="outlined"
                 color="secondary"
                 className={classes.button}
-                onClick={getAllHandler}
+                onClick={() => addHandler(addItem)}
+                disabled
               >
-                getAll
+                  addNew
               </Button>
-              {this.props.block === 1 ? (
-                <Button
-                  variant="outlined"
-                  color="secondary"
-                  className={classes.button}
-                  onClick={() => addHandler(this.state.addItem)}
-                  disabled
-                >
-                  addNew
-                </Button>
-              ) : (
-                <Button
-                  variant="outlined"
-                  color="secondary"
-                  className={classes.button}
-                  onClick={() => addHandler(this.state.addItem)}
-                >
-                  addNew
-                </Button>
-              )}
-              <br />
-              <TextField
-                id="standard-name"
-                label="name"
-                name="title"
-                type="text"
-                value={this.state.addItem.title}
-                onChange={this.addNewHandler}
-              />
-              <TextField
-                id="standard-name"
-                label="release date"
-                name="release"
-                type="number"
-                value={this.state.addItem.release}
-                onChange={this.addNewHandler}
-              />{' '}
-              <br />
-              <TextField
-                id="standard-name"
-                label="format"
-                name="format"
-                type="text"
-                value={this.state.addItem.format}
-                onChange={this.addNewHandler}
-              />
-              <TextField
-                id="standard-name"
-                label="stars"
-                name="stars"
-                type="text"
-                value={this.state.addItem.stars}
-                onChange={this.addNewHandler}
-              />
-            </div>{' '}
-            <div className={classes.column}>
+            ) : (
               <Button
                 variant="outlined"
-                color="primary"
-                onClick={() => byActorHandler(this.state.byActorInput)}
+                color="secondary"
+                className={classes.button}
+                onClick={() => addHandler(addItem)}
               >
+                  addNew
+              </Button>
+            )}
+            <br />
+            <TextField
+              id="standard-name"
+              label="name"
+              name="title"
+              type="text"
+              value={addItem.title}
+              onChange={addNewHandler}
+            />
+            <TextField
+              id="standard-name"
+              label="release date"
+              name="release"
+              type="number"
+              value={addItem.release}
+              onChange={addNewHandler}
+            />{' '}
+            <br />
+            <TextField
+              id="standard-name"
+              label="format"
+              name="format"
+              type="text"
+              value={addItem.format}
+              onChange={addNewHandler}
+            />
+            <TextField
+              id="standard-name"
+              label="stars"
+              name="stars"
+              type="text"
+              value={addItem.stars}
+              onChange={addNewHandler}
+            />
+          </div>{' '}
+          <div className={classes.column}>
+            <Button
+              variant="outlined"
+              color="primary"
+              onClick={() => byActorHandler(byActorInput)}
+            >
                 searchByActor
-              </Button>
-              <TextField
-                id="standard-name"
-                label="actor's name"
-                type="text"
-                value={this.state.byActorInput}
-                onChange={this.byActorInputHandler}
-              />{' '}
-              <br />
-              <Button
-                variant="outlined"
-                color="primary"
-                onClick={() => byNameHandler(this.state.byNameInput)}
-              >
+            </Button>
+            <TextField
+              id="standard-name"
+              label="actor's name"
+              type="text"
+              value={byActorInput}
+              onChange={byActorInputHandler}
+            />{' '}
+            <br />
+            <Button
+              variant="outlined"
+              color="primary"
+              onClick={() => byNameHandler(byNameInput)}
+            >
                 searchByName
-              </Button>
-              <TextField
-                id="standard-name"
-                label="film's name"
-                type="text"
-                value={this.state.byNameInput}
-                onChange={this.byNameInputHandler}
-              />
-              <br />
-              <Button variant="outlined" color="secondary" onClick={sortDownHandler}>
+            </Button>
+            <TextField
+              id="standard-name"
+              label="film's name"
+              type="text"
+              value={byNameInput}
+              onChange={byNameInputHandler}
+            />
+            <br />
+            <Button variant="outlined" color="secondary" onClick={sortDownHandler}>
                 ↑
-              </Button>
-              <Button variant="outlined" color="secondary" onClick={sortUpHandler}>
+            </Button>
+            <Button variant="outlined" color="secondary" onClick={sortUpHandler}>
                 ↓
-              </Button>
-            </div>{' '}
-            <div className={classes.column}>
-              <ReactFileReader fileTypes={['.txt']} handleFiles={handleFiles} multipleFiles={false}>
-                <Button variant="outlined" color="secondary">
+            </Button>
+          </div>{' '}
+          <div className={classes.column}>
+            <ReactFileReader fileTypes={['.txt']} handleFiles={handleFiles} multipleFiles={false}>
+              <Button variant="outlined" color="secondary">
                   Upload
-                </Button>
-              </ReactFileReader>
-            </div>
-          </ExpansionPanelDetails>
-          {/* <Divider />
+              </Button>
+            </ReactFileReader>
+          </div>
+        </ExpansionPanelDetails>
+        {/* <Divider />
           <ExpansionPanelActions>
             <Button size="small">Cancel</Button>
             <Button size="small" color="primary">
               Save
             </Button>
           </ExpansionPanelActions>*/}
-        </ExpansionPanel>
-      </div>
-    );
-  }
+      </ExpansionPanel>
+    </div>
+  );
 }
 
 export default withStyles(styles)(Index);
