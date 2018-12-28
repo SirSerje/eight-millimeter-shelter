45e2c84d618e29f67a49949718978398cdd3bc27
SirSerje <sir.serje@gmail.com>
2018-12-28 11:31:10 +0200

    components update

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,11 +1,9 @@
-import React from "react";
-import PropTypes from "prop-types";
-import {Provider} from "react-redux";
-import {BrowserRouter, Link, Route} from "react-router-dom";
-import App from "./App";
-import {getById} from "../utils";
-import axios from "axios";
-import PATH from "../constants/path";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import App from './App';
+import { getById } from '../utils';
 
 const Root = ({ store }) => (
   <BrowserRouter>
@@ -14,7 +12,7 @@ const Root = ({ store }) => (
         <Route exact path="/" component={App} store={store} />
         <Route path="/final-app" component={() => <div>test route</div>} />
         {/*FIXME: if path with movie will change to host/some/:id it will crash, because app.js is looked in host/some*/}
-        <Route path="/:id" component = {RenderPropApproach} something="foo"/>
+        <Route path="/:id" component={RenderPropApproach} something="foo" />
       </React.Fragment>
     </Provider>
   </BrowserRouter>
@@ -22,6 +20,41 @@ const Root = ({ store }) => (
 Root.propTypes = {
   store: PropTypes.object.isRequired,
 };
+
+const RenderPropApproach = ({ data, isLoading, error }) => (
+  <Fetcher url={86}>
+    {({ data, isLoading, error }) => {
+      if (!data) {
+        return <p>No data yet ...</p>;
+      }
+
+      if (error) {
+        return <p>{error.message}</p>;
+      }
+
+      if (isLoading) {
+        return <p>Loading ...</p>;
+      }
+
+      if (data && data.message) {
+        const { release, stars, title, format } = data.message;
+        return (
+          <div>
+            {`${title} (${release}) ${format}`}
+            <p>
+              stars:
+              {stars.length > 0 && stars.map(i => <span>{i} </span>)}
+            </p>
+          </div>
+        );
+      }
+
+      return (
+        <p>unfortunately, no data, smth happened</p>
+      );
+    }}
+  </Fetcher>
+);
 
 class Fetcher extends React.Component {
   constructor(props) {
@@ -37,54 +70,31 @@ class Fetcher extends React.Component {
   componentDidMount() {
     this.setState({ isLoading: true });
 
-    axios.get(this.props.url)
-      .then(result => this.setState({
-        data: result.data,
-        isLoading: false
-      }))
-      .catch(error => this.setState({
-        error,
-        isLoading: false
-      }));
+    getById(this.props.url)
+      .then(result =>
+        this.setState({
+          data: result.data,
+          isLoading: false,
+        })
+      )
+      .catch(error =>
+        this.setState({
+          error,
+          isLoading: false,
+        })
+      );
   }
 
   render() {
-    return this.props.children(this.state);
+    return (
+      <div>
+        <Link to="/">
+          <button>Back Home</button>
+        </Link>
+        {this.props.children(this.state)}
+      </div>
+    );
   }
 }
-
-const RenderPropApproach = ({ data, isLoading, error }) =>
-// TODO: id should come from props;
-//Props come from Fetcher
-
-  <Fetcher url={`${PATH}/${86}`}>
-    {console.log('props', data, isLoading, error)}
-    {({ data, isLoading, error }) => {
-      if (!data) {
-        return <p>No data yet ...</p>;
-      }
-
-      if (error) {
-        return <p>{error.message}</p>;
-      }
-
-      if (isLoading) {
-        return <p>Loading ...</p>;
-      }
-      let a = data && data.message;
-      return (
-        <React.Fragment>
-          <Link to="/"><button>Back Home</button></Link>
-          <ul>
-            <li>{a.format}</li>
-            <li>{a.release}</li>
-            <li>{a.stars}</li>
-            <li>{a.title}</li>
-          </ul>
-        </React.Fragment>
-      );
-    }}
-  </Fetcher>;
-
 
 export default Root;
