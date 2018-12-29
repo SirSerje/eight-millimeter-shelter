847c9a9403e322224cfa844c1f0dd076232996cc
SirSerje <sir.serje@gmail.com>
2018-12-29 15:32:00 +0200

    move mongo stuff to separate file

--- src/server/index.js
+++ src/server/index.js
@@ -6,27 +6,6 @@ const router = express.Router();
 //config
 const PORT = 4125;
 
-//PoC for mongoDB connection
-const uri = 'mongodb+srv://X:Y@cluster0-cpecu.gcp.mongodb.net/test'; //TODO: insert you log:pass
-const MongoClient = require('mongodb').MongoClient;
-const client = new MongoClient(uri, { useNewUrlParser: true });
-
-client.connect(function(err, client) {
-  if (err) {
-    console.log('Error occurred while connecting to MongoDB Atlas...\n', err);
-  }
-
-  const collection = client.db('users').collection('users');
-
-  collection.findOne({ _id: '1' }, function(err, result) {
-    if (err) throw err;
-    console.log('result: ', result);
-  });
-
-  client.close();
-});
-
-/*
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -56,4 +35,4 @@ app.use('/api', router);
 
 const index = app.listen(PORT, function() {
   console.log('app running on port.', index.address().port);
-});*/
+});
