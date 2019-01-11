34f68ee891434816a759eca6c8db0b74e91b8219
SirSerje <sir.serje@gmail.com>
2019-01-12 00:52:19 +0200

    import config into server

--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
-
+const config = require('./config');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
